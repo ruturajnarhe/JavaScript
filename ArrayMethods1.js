f00581e4814b0eed650342464d1a8716674c0fab
ruturajnarhe <165364911+ruturajnarhe@users.noreply.github.com>
2024-07-22 00:02:44 +0530

    new code

--- ArrayMethods1.js
+++ ArrayMethods1.js
@@ -37,3 +37,11 @@ output = arr.reduce((prev, curr) => {
     return prev > curr ? prev : curr;
 });
 console.log(output);
+
+
+// filter that scores 90+
+let marks = [97, 64, 32, 49, 99, 96, 86];
+let toppers = marks.filter((val) => {
+    return val > 90;
+});
+console.log(toppers);
