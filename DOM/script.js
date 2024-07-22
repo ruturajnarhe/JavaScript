f8f081ec5252e00671de1de609bbdf250a402390
ruturajnarhe <165364911+ruturajnarhe@users.noreply.github.com>
2024-07-22 19:51:52 +0530

    new code

---
+++ DOM/script.js
@@ -0,0 +1,13 @@
+console.log("Hello!");
+let heading = document.getElementById("heading");
+console.dir(heading);
+console.log(heading);
+
+
+let classes = document.getElementsByClassName("heading-class");
+console.dir(classes);
+console.log(classes);
+
+let parahs = document.getElementsByTagName("p");
+console.dir(parahs);
+console.log(parahs);
